7317eaae912a1509f4a2ae81fbad90e910271fa6
Adam Ottvar <adam.ottvar@gmail.com>
2019-12-01 20:42:44 +0100

    Updated with recursive instead of while.

--- 1dec/1dec.py
+++ 1dec/1dec.py
@@ -7,15 +7,20 @@ def calculate_fuel(mass):
     fuel = math.floor(mass/3) - 2
     return fuel
 
+def recursive_fuel(mass):
+	unit_fuel = calculate_fuel(mass)
+	if unit_fuel <= 0:
+		return 0
+	else:
+		return unit_fuel + recursive_fuel(unit_fuel)
+
 with open('1dec_input.txt') as input_file:
     for line in input_file:
         mass = int(line)
         unit_fuel = calculate_fuel(mass)
         sum_of_fuel += unit_fuel
-        while unit_fuel > 0:
-            unit_fuel = calculate_fuel(unit_fuel)
-            sum_of_fuel_for_fuel += unit_fuel if unit_fuel > 0 else 0       
-
+        sum_of_fuel_for_fuel += recursive_fuel(unit_fuel)
+        
 print("Part 1:")
 print(sum_of_fuel)
 print("Part 2:")
