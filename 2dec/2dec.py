00fc991c682101dea8cbfab98f3c1eb2c00ffe85
Adam Ottvar <adam.ottvar@gmail.com>
2019-12-02 18:57:06 +0100

    Added solution for second problem

--- 2dec/2dec.py
+++ 2dec/2dec.py
@@ -1,10 +1,8 @@
 #!python3
 
-def execute_one_op(sequence_index):
+def execute_one_op(sequence, sequence_index):
     opcode = sequence[sequence_index]
     if opcode == 99:
-        print("Program halted")
-        print(sequence[0])
         return -1
     elif opcode == 1:
         arg1 = sequence[sequence[sequence_index+1]]
@@ -19,17 +17,51 @@ def execute_one_op(sequence_index):
         sequence[result_addr] = arg1 * arg2
         return sequence_index + 4
     else:
+        return -99
+
+def execute_complete_program(seq):
+    seq_index = 0
+    
+    while seq_index >= 0:
+        seq_index = execute_one_op(seq, seq_index)
+
+    if seq_index == -1:
+        return seq[0]
+    else:
         print("Something went wrong!")
         return -1
 
+
 with open('input2.txt') as input_file:
     intcode = input_file.read().strip().split(',')
-    sequence = [int(i) for i in intcode]
+    sequence_orig = [int(i) for i in intcode]
 
 # First task
+sequence = sequence_orig.copy()
 sequence[1] = 12
 sequence[2] = 2
+first_answer = execute_complete_program(sequence)
+print("=== First answer: ")
+print(first_answer)
 
-sequence_index = 0
-while sequence_index >= 0:
-    sequence_index = execute_one_op(sequence_index)
+# Second task
+list_of_nouns = range(100)
+list_of_verbs = range(100)
+
+for noun in list_of_nouns:
+    for verb in list_of_verbs:
+        sequence = sequence_orig.copy()
+        sequence[1] = noun
+        sequence[2] = verb
+        second_answer = execute_complete_program(sequence)
+        
+        if second_answer == 19690720:
+            print("=== Second answer: ")
+            print("Noun: {}   Verb: {}".format(noun, verb))
+            print("Solution: {}".format(100 * noun + verb))
+            break
+    else:
+        # Continue if the inner loop wasn't broken.
+        continue
+    # Inner loop was broken, break the outer.
+    break
