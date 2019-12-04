b8f52d15c5acfa6b13244fd3efd574b053f1d7d7
Adam Ottvar <adam.ottvar@gmail.com>
2019-12-04 13:32:10 +0100

    Added code for second task

--- 4dec/4dec.py
+++ 4dec/4dec.py
@@ -2,10 +2,15 @@
 
 def check_combination(number):
     unique_digits = []
+    repeated_digits = {}
     previous_digit = 0
+    repeated_digit_counter = 0
     for digit in [int(d) for d in str(number)]:
+        if digit in repeated_digits:
+            repeated_digits[digit] = repeated_digits[digit] + 1
         if digit not in unique_digits:
             unique_digits.append(digit)
+            repeated_digits[digit] = 1
         if digit < previous_digit:
             return False
         previous_digit = digit
@@ -13,12 +18,17 @@ def check_combination(number):
     if len(unique_digits) == 6:
         return False
 
-    return True
+    one_double_digit = False
+    for digit in repeated_digits:
+        if repeated_digits[digit] == 2:
+            one_double_digit =  True
 
-def first_task(): 
-    number_of_passwords = 0
-    for i in range(108457, 562041+1):
-        if check_combination(i):
-            number_of_passwords += 1
+    return one_double_digit
 
-    print(number_of_passwords)
+
+number_of_passwords = 0
+for i in range(108457, 562041+1):
+    if check_combination(i):
+        number_of_passwords += 1
+
+print(number_of_passwords)
