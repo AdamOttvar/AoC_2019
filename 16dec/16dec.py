f15cb4b7c007cbd41c23dbdc9b5bbe85b3de6c96
Adam Ottvar <adam.ottvar@gmail.com>
2019-12-21 09:07:00 +0100

    Code for second, not working.

--- 16dec/16dec.py
+++ 16dec/16dec.py
@@ -30,4 +30,41 @@ def first_task():
 
     print(output_list[:8])
 
-first_task()
+
+def second_task():
+    
+    with open('input16.txt') as input_file:
+        signal_input = input_file.read().strip()
+    
+    #signal_input = '69317163492948606335995924319873'
+
+    signal = [int(x) for x in signal_input]
+    output_list = [0]*len(signal)
+    base_pattern = [0, 1, 0, -1]
+
+    nbr_of_phases = 100
+    for phase in range(0,nbr_of_phases):
+        for index in range(0,len(output_list)):
+            repeated_pattern = []
+            for index2 in base_pattern:
+                repeat = [base_pattern[index2]]*(index+1)
+                repeated_pattern.extend(repeat)
+            pattern = repeated_pattern[1:]
+            while len(pattern) < len(output_list):
+                pattern.extend(repeated_pattern)
+            pattern = pattern[0:len(output_list)]
+            
+            result = 0
+            for index3 in range(index,len(signal)):
+                result += pattern[index3]*signal[index3]
+
+            output_list[index] = abs(result)%10
+
+        signal = output_list.copy()
+
+    message_start = int(''.join(str(x) for x in signal_input[:7]))
+    print(message_start)
+    print(output_list[:8])
+
+#first_task()
+second_task()
