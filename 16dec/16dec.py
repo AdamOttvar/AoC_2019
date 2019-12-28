2e26f609e4fa87624e709d5a17e2504dc0197aee
Adam Ottvar <adam.ottvar@gmail.com>
2019-12-28 13:26:45 +0100

    Problem 2 solved

--- 16dec/16dec.py
+++ 16dec/16dec.py
@@ -37,34 +37,27 @@ def second_task():
         signal_input = input_file.read().strip()
     
     #signal_input = '69317163492948606335995924319873'
-
+    #signal_input = '1111111111'
+    message_start = int(''.join(str(x) for x in signal_input[:7]))
     signal = [int(x) for x in signal_input]
+    signal = signal*10000
     output_list = [0]*len(signal)
     base_pattern = [0, 1, 0, -1]
 
+    needed_index_start = 2*message_start - len(signal)
+
     nbr_of_phases = 100
     for phase in range(0,nbr_of_phases):
-        for index in range(0,len(output_list)):
-            repeated_pattern = []
-            for index2 in base_pattern:
-                repeat = [base_pattern[index2]]*(index+1)
-                repeated_pattern.extend(repeat)
-            pattern = repeated_pattern[1:]
-            while len(pattern) < len(output_list):
-                pattern.extend(repeated_pattern)
-            pattern = pattern[0:len(output_list)]
-            
-            result = 0
-            for index3 in range(index,len(signal)):
-                result += pattern[index3]*signal[index3]
-
+        print(phase)
+        result = 0
+        for index in range(len(output_list)-1, needed_index_start, -1):
+            result += signal[index]
             output_list[index] = abs(result)%10
 
         signal = output_list.copy()
 
-    message_start = int(''.join(str(x) for x in signal_input[:7]))
     print(message_start)
-    print(output_list[:8])
+    print(output_list[message_start:message_start+8])
 
 #first_task()
 second_task()
